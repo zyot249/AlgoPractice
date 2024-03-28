2a7abbad9520d919d913d14f795d67d6dceec0e4
NguyenDung249 <dung.nguyen@sipher.xyz>
2024-03-29 00:13:45 +0700

    longest consecutive sequence

---
+++ src/arrayandhasing/practice/longest_consecutive_sequence.go
@@ -0,0 +1,79 @@
+package practice
+
+/*
+Problem:
+Ref: https://leetcode.com/problems/longest-consecutive-sequence/
+Given:
+- An unsorted array of integers
+
+Return:
+- The length of the longest consecutive sequence
+
+Constraints:
+- arr length: [0, 10^5]
+- arr[i]: [-10^9, 10^9]
+- Time complexity: O(n)
+*/
+
+/*
+First approach:
+Firstly, we convert the array into a set of numbers for checking the existence
+Secondly, we iterate all the elements in array.
+For each element, if it isn't a start point of sequence, skip it.
+If it is a start point of sequence, find next element in its sequence until reaching end and calculate the length of sequence
+
+Time complexity: O(n)
+Space complexity: O(n)
+
+Improvement 1:
+In practice, we found that above solution spends ~1900ms to finish test cases in Leetcode for golang
+*/
+func longestConsecutive(nums []int) int {
+	set := make(map[int]bool)
+	for _, num := range nums {
+		set[num] = true
+	}
+
+	maxLength := 0
+	//Solution 1
+	//for _, num := range nums {
+	//	if set[num-1] {
+	//		continue
+	//	}
+	//
+	//	length := 1
+	//	for next := num + 1; set[next] == true; next++ {
+	//		length++
+	//	}
+	//
+	//	if length > maxLength {
+	//		maxLength = length
+	//	}
+	//}
+
+	//Solution 2
+	for _, num := range nums {
+		if !set[num] {
+			continue
+		}
+
+		delete(set, num)
+		length := 1
+
+		for next := num + 1; set[next] == true; next++ {
+			delete(set, next)
+			length++
+		}
+
+		for prev := num - 1; set[prev] == true; prev-- {
+			delete(set, prev)
+			length++
+		}
+
+		if length > maxLength {
+			maxLength = length
+		}
+	}
+
+	return maxLength
+}
