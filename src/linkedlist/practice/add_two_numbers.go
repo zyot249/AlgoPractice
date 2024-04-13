1526403974ed865575dd61636b3791c5909b6935
NguyenDung249 <dung.nguyen@sipher.xyz>
2024-04-13 21:41:53 +0700

    add two numbers

---
+++ src/linkedlist/practice/add_two_numbers.go
@@ -0,0 +1,54 @@
+package practice
+
+/*
+Problem:
+Ref: https://leetcode.com/problems/add-two-numbers/description/
+
+Given:
+- 2 linked lists that store the digits of 2 non-negative numbers in reverse order.
+
+Return:
+- Add 2 number and return the linked list that stores the result's digits in reverse order.
+
+Constraints:
+- length of list: [1, 100].
+- It is guaranteed that the list represents a number that does not have leading zeros.
+*/
+
+/*
+First approach:
+I will iterate 2 lists at the same time and I will add 2 digits at the same position and store the result to response list
+If sum of 2 digits is greater than 9, the digit in result will be sum mod 10 and bonus for next position will be sum / 10
+*/
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	res := &ListNode{}
+
+	ptr1, ptr2 := l1, l2
+	cur := res
+	bonus := 0
+	for ptr1 != nil || ptr2 != nil {
+		digit1, digit2 := 0, 0
+		if ptr1 != nil {
+			digit1 = ptr1.Val
+			ptr1 = ptr1.Next
+		}
+
+		if ptr2 != nil {
+			digit2 = ptr2.Val
+			ptr2 = ptr2.Next
+		}
+
+		sum := digit1 + digit2 + bonus
+		cur.Next = &ListNode{Val: sum % 10, Next: nil}
+		bonus = sum / 10
+
+		cur = cur.Next
+	}
+
+	if bonus > 0 {
+		cur.Next = &ListNode{Val: bonus}
+	}
+
+	return res.Next
+}
---
+++ src/linkedlist/practice/add_two_numbers_test.go
@@ -0,0 +1,41 @@
+package practice
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test1",
+			args: args{l1: CreateLinkedList([]int{2, 4, 3}), l2: CreateLinkedList([]int{5, 6, 4})},
+			want: CreateLinkedList([]int{7, 0, 8}),
+		},
+		{
+			name: "test2",
+			args: args{l1: CreateLinkedList([]int{0}), l2: CreateLinkedList([]int{0})},
+			want: CreateLinkedList([]int{0}),
+		},
+		{
+			name: "test3",
+			args: args{l1: CreateLinkedList([]int{9, 9, 9, 9, 9, 9, 9}), l2: CreateLinkedList([]int{9, 9, 9, 9})},
+			want: CreateLinkedList([]int{8, 9, 9, 9, 0, 0, 0, 1}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
