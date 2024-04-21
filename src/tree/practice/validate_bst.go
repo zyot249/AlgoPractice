22b51db78908752a556cd6c28e70221334cf13d3
NguyenDung249 <dung.nguyen@sipher.xyz>
2024-04-21 23:19:07 +0700

    validate bst

--- src/tree/practice/validate_bst.go
+++ src/tree/practice/validate_bst.go
@@ -3,6 +3,30 @@ package practice
 import "math"
 import . "AlgoPractice/src/tree/structure"
 
+/*
+Problem:
+Ref: https://leetcode.com/problems/validate-binary-search-tree/description/
+
+Given:
+- a root of binary tree
+
+Return:
+- True if it is a binary search tree and false otherwise
+
+Constraints:
+- number of nodes: [1, 10^4]
+- node val: [-2^31, 2^31 - 1]
+*/
+
+/*
+First approach:
+When I traverse the tree in preorder, I can find the range of possible value of next node.
+Value of a node must be in range
+
+Time complexity: O(n)
+Space complexity: O(n)
+*/
+
 func isValidBST(root *TreeNode) bool {
 	return checkBST(root, math.Inf(-1), math.Inf(1))
 }
